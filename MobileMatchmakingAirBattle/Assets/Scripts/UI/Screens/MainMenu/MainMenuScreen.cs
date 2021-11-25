8297df20171859cc35cf126ef0f7f36a3364bc1b
Jeffy94 <hodevg94@gmail.com>
2021-11-25 15:15:52 +0300

    UI And Google
    UI And  Google

--- MobileMatchmakingAirBattle/Assets/Scripts/UI/Screens/MainMenu/MainMenuScreen.cs
+++ MobileMatchmakingAirBattle/Assets/Scripts/UI/Screens/MainMenu/MainMenuScreen.cs
@@ -1,21 +1,17 @@
 using Assets.Scripts.Core;
 using Assets.Scripts.Utils;
-using Network.Google;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace UI.Screens.MainMenu
 {
     public class MainMenuScreen : BaseScreen
     {
         [SerializeField] private PlayerPanel _playerPanel;
-        [SerializeField] private Button _leaderBoardButton;
         public override ScreenType Type => ScreenType.MainMenu;
 
         private void OnEnable()
         {
             _playerPanel.Config();
-            _leaderBoardButton.gameObject.SetActive(GooglePlayManager.IsLoad);
         }
 
         public void SwitchToSearchScreen() => ScreenHolder.SetCurrentScreen(ScreenType.Search).ShowScreen();
@@ -25,7 +21,5 @@ namespace UI.Screens.MainMenu
         public void SwitchToOptionsScreen() => ScreenHolder.SetCurrentScreen(ScreenType.Options).ShowScreen();
 
         public void SwitchToShopScreen() => ScreenHolder.SetCurrentScreen(ScreenType.Shop).ShowScreen();
-
-        //public void Exit() => ScreenHolder.SetCurrentScreen(ScreenType.Search).ShowScreen();
     }
 }
--- MobileMatchmakingAirBattle/Assets/Scripts/UI/Screens/MainMenu/PlayerPanel.cs
+++ MobileMatchmakingAirBattle/Assets/Scripts/UI/Screens/MainMenu/PlayerPanel.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using Assets.Scripts.Core;
-using Assets.Scripts.Utils;
 using Network.Google;
 using TMPro;
 using UnityEngine;
@@ -10,8 +8,6 @@ namespace UI.Screens.MainMenu
 {
     public class PlayerPanel : MonoBehaviour
     {
-        [SerializeField] private Transform _playerInfoPanel;
-        [SerializeField] private Transform _googlePlayButton;
         [SerializeField] private Image _playerAvatar;
         [SerializeField] private TMP_Text _playerName;
 
@@ -19,15 +15,8 @@ namespace UI.Screens.MainMenu
         {
             if (GooglePlayManager.IsLoad)
             {
-                _playerInfoPanel.gameObject.SetActive(true);
-                _googlePlayButton.gameObject.SetActive(false);
                 _playerName.text = Social.localUser.userName;
                 StartCoroutine(nameof(LocalImage));
-            }
-            else
-            {
-                _playerInfoPanel.gameObject.SetActive(false);
-                _googlePlayButton.gameObject.SetActive(true);
             }
         }
 
@@ -38,11 +27,6 @@ namespace UI.Screens.MainMenu
             _playerAvatar.sprite = Sprite.Create(Social.localUser.image,
                 new Rect(0.0f, 0.0f, Social.localUser.image.width, Social.localUser.image.height),
                 Vector2.zero);
-        }
-
-        public void GoogleSignInOnClick()
-        {
-            ScreenHolder.SetCurrentScreen(ScreenType.Load).ShowScreen();
         }
     }
 }
