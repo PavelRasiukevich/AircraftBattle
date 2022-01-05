ae2b0befb22bfc1346d57e3af946d43a29e80253
Jeffy94 <hodevg94@gmail.com>
2022-01-05 18:23:50 +0300

    AIRBATTLE-42-Shop
    AIRBATTLE-42-Shop

--- MobileMatchmakingAirBattle/Assets/Scripts/UI/Screens/Shop/Elements/ShopItemLine.cs
+++ MobileMatchmakingAirBattle/Assets/Scripts/UI/Screens/Shop/Elements/ShopItemLine.cs
@@ -17,15 +17,16 @@ namespace UI.Screens.Shop.Elements
         [SerializeField] private TMP_Text _name;
         [SerializeField] private TMP_Text _hp;
         [SerializeField] private TMP_Text _speed;
-        [SerializeField] private TMP_Text _firePower;
+        [SerializeField] private TMP_Text _mobility;
 
         // UI
         [SerializeField] private Image _background;
         [SerializeField] private Sprite _backgroundLight;
         [SerializeField] private Sprite _backgroundDark;
         [SerializeField] private Button _buyButton;
+        [SerializeField] private Button _infoButton;
         [SerializeField] private Button _selectButton;
-        [SerializeField] private Image _isSelected;
+        [SerializeField] private Image _isSelectedIcon;
 
         #region UNITY
 
@@ -33,6 +34,7 @@ namespace UI.Screens.Shop.Elements
         {
             //  _buyButton.onClick.AddListener(BuyOnClick);
             _selectButton.onClick.AddListener(SelectOnClick);
+            _infoButton.onClick.AddListener(InfoOnClick);
         }
 
         #endregion
@@ -47,20 +49,22 @@ namespace UI.Screens.Shop.Elements
             _name.text = planeInfo.DisplayName;
             _hp.text = planeInfo.PlanePrefab.DataModel.Hp.ToString(CultureInfo.InvariantCulture);
             _speed.text = planeInfo.PlanePrefab.DataModel.Speed.MoveSpeed.ToString(CultureInfo.InvariantCulture);
-            _firePower.text = planeInfo.FirePower.ToString(CultureInfo.InvariantCulture);
+            _mobility.text = planeInfo.PlanePrefab.DataModel.Speed.RotationSpeed.ToString(CultureInfo.InvariantCulture);
             // UI
             if (GameData.Inst.CurrentPlane.ID == planeInfo.ID)
             {
                 //Самолет выбран
-                _isSelected.gameObject.SetActive(true);
+                _isSelectedIcon.gameObject.SetActive(true);
                 _selectButton.gameObject.SetActive(false);
+                _infoButton.gameObject.SetActive(false);
                 _buyButton.gameObject.SetActive(false);
                 _background.sprite = _backgroundLight;
             }
             else
             {
-                _isSelected.gameObject.SetActive(false);
+                _isSelectedIcon.gameObject.SetActive(false);
                 _selectButton.gameObject.SetActive(true);
+                _infoButton.gameObject.SetActive(true);
                 _buyButton.gameObject.SetActive(false);
                 _background.sprite = _backgroundDark;
             }
@@ -74,6 +78,12 @@ namespace UI.Screens.Shop.Elements
         {
             GameData.Inst.SelectPlane(_id);
             ScreenHolder.SetCurrentScreen(ScreenType.MainMenu).ShowScreen();
+        }
+
+        private void InfoOnClick()
+        {
+            GameData.Inst.SelectShopPlane(_id);
+            ScreenHolder.SetCurrentScreen(ScreenType.ShopView).ShowScreen();
         }
 
         #endregion
