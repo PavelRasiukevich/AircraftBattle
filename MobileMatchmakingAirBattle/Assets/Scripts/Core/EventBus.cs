08d24277a6389a0dcb6ea3bc862a1066c089f618
Jeffy94 <hodevg94@gmail.com>
2022-01-31 12:32:34 +0300

    AIRBATTLE-47-Fix-Event-Bus
    AIRBATTLE-47-Fix-Event-Bus

--- MobileMatchmakingAirBattle/Assets/Scripts/Core/EventBus.cs
+++ MobileMatchmakingAirBattle/Assets/Scripts/Core/EventBus.cs
@@ -9,55 +9,56 @@ namespace Core
     //TODO: Проверку на удаление во время исполнения/foreach
     public static class EventBus
     {
-        private static Dictionary<Type, List<ISubscriber>> subscribers = new Dictionary<Type, List<ISubscriber>>();
+        private static Dictionary<Type, List<ISubscriber>> _subscribers = new Dictionary<Type, List<ISubscriber>>();
 
         public static void Subscribe<TSubscriber>(TSubscriber subscriber) where TSubscriber : class, ISubscriber
         {
-            foreach (Type t in GetSubscriberInterfaces(subscriber.GetType()))
+            foreach (Type t in GetInterfaces(subscriber.GetType()))
             {
-                if (!subscribers.ContainsKey(t))
-                    subscribers[t] = new List<ISubscriber>();
-                subscribers[t].Add(subscriber);
+                if (!_subscribers.ContainsKey(t))
+                    _subscribers[t] = new List<ISubscriber>();
+                _subscribers[t].Add(subscriber);
             }
         }
 
         public static void Unsubscribe<TSubscriber>(TSubscriber subscriber) where TSubscriber : class, ISubscriber
         {
-            foreach (Type t in GetSubscriberInterfaces(subscriber.GetType()))
-                if (subscribers.ContainsKey(t))
-                    subscribers[t].Remove(subscriber);
+            foreach (Type t in GetInterfaces(subscriber.GetType()))
+                if (_subscribers.ContainsKey(t))
+                    _subscribers[t].Remove(subscriber);
         }
 
         public static void InvokeEvent<TSubscriber>(Action<TSubscriber> action) where TSubscriber : class, ISubscriber
         {
-            foreach (ISubscriber subscriber in subscribers[typeof(TSubscriber)])
+            if (_subscribers.TryGetValue(typeof(TSubscriber), out var list))
             {
-                try
+                foreach (ISubscriber subscriber in list)
                 {
-                    action.Invoke(subscriber as TSubscriber);
+                    try
+                    {
+                        action.Invoke(subscriber as TSubscriber);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogError(e);
+                    }
                 }
-                catch (Exception e)
-                {
-                    Debug.LogError(e);
-                }
+
+                LogEventBus();
             }
-            LogEventBus();
         }
 
-        private static List<Type> GetSubscriberInterfaces(Type subscriberType)
-        {
-            List<Type> subscriberTypes = subscriberType
+        private static List<Type> GetInterfaces(Type subscriberType) =>
+            subscriberType
                 .GetInterfaces()
                 .Where(it =>
                     it.GetInterfaces().Contains(typeof(ISubscriber)) &&
                     it != typeof(ISubscriber))
                 .ToList();
-            return subscriberTypes;
-        }
 
         private static void LogEventBus()
         {
-            foreach (KeyValuePair<Type, List<ISubscriber>> pair in subscribers)
+            foreach (KeyValuePair<Type, List<ISubscriber>> pair in _subscribers)
             {
                 Debug.Log($"{pair.Key.Name}  :");
                 if (pair.Value.Count == 0)
