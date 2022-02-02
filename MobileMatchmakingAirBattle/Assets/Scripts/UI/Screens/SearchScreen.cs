219218f7e81b79f2163d24119e4c2a86e3cefc75
Jeffy94 <hodevg94@gmail.com>
2022-02-02 22:02:32 +0300

    FIX: Add Authentication Type None - for test

--- MobileMatchmakingAirBattle/Assets/Scripts/UI/Screens/SearchScreen.cs
+++ MobileMatchmakingAirBattle/Assets/Scripts/UI/Screens/SearchScreen.cs
@@ -1,9 +1,5 @@
-using System;
 using Core.Base;
 using Managers.Network.Launcher;
-using Photon.Pun;
-using TMPro;
-using UnityEngine;
 using Utils.Enums;
 
 namespace Assets.Scripts.UI.Screens
@@ -12,25 +8,8 @@ namespace Assets.Scripts.UI.Screens
     {
         public override ScreenType Type => ScreenType.Search;
 
-        [SerializeField] private TMP_Text _playerID;
-
         #region UNITY
-
-        private void Update()
-        {
-            try
-            {
-                if (PhotonNetwork.LocalPlayer != null)
-                {
-                    _playerID.text = $"ID: {PhotonNetwork.LocalPlayer.UserId}";
-                }
-            }
-            catch (Exception e)
-            {
-                // ignored
-            }
-        }
-
+        
         #endregion
 
         #region OnClick
